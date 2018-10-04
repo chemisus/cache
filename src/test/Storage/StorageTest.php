57b1f94320fedcd7ecc86c75eca03ec34f120aca
Terrence Howard <chemisus@gmail.com>
2018-10-04 18:11:17 -0400

    add tests

---
+++ src/test/Storage/StorageTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Chemisus\Storage;
+
+use PHPUnit_Framework_TestCase;
+
+abstract class StorageTest extends PHPUnit_Framework_TestCase
+{
+    private $keys;
+    private $entries;
+
+    public abstract function factory();
+
+    public function data()
+    {
+        return array(
+            array(static::factory()),
+        );
+    }
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->keys = array("a", "b", "c", "x", "y", "z");
+        $this->entries = array("a" => "A", "b" => "B", "c" => "C");
+    }
+
+    /**
+     * @dataProvider data
+     * @param Storage $storage
+     * @return Storage
+     */
+    public function testFactory(Storage $storage)
+    {
+        $actual = $storage->get($this->keys);
+        $this->assertEmpty($actual, "Storage should be empty to begin with.");
+        return $storage;
+    }
+
+    /**
+     * @dataProvider data
+     * @param Storage $storage
+     */
+    public function testPut(Storage $storage)
+    {
+        $storage->put($this->entries);
+        $actual = $storage->get($this->keys);
+        self::assertNotEmpty($actual, "Storage should store and return entries.");
+    }
+
+    /**
+     * @depends testPut
+     * @param Storage $storage
+     */
+    public function testGet(Storage $storage)
+    {
+        $expect = $this->entries;
+        $actual = $storage->get($this->keys);
+        self::assertEquals($expect, $actual, "Storage should return entries.");
+    }
+
+    /**
+     * @depends testGet
+     * @param Storage $storage
+     */
+    public function testDelete(Storage $storage)
+    {
+        $storage->delete($this->keys);
+        $actual = $storage->get($this->keys);
+        self::assertEmpty($actual, "Storage should delete entries by key.");
+    }
+}
